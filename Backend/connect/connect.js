4457092d65b22237b7e66f34392d5f81a4add361
nayyelin-2002 <yelinnay4@gmail.com>
2025-03-15 10:11:01 +0700

    changes

--- Backend/connect/connect.js
+++ Backend/connect/connect.js
@@ -1,20 +1,41 @@
+// const mysql = require("mysql2");
+// //++++++++++++++++++++++++++++++++++++++++++
+// // DB Connection
+// //++++++++++++++++++++++++++++++++++++++++++
+// require("dotenv").config();
+
+// const connection = mysql.createConnection(process.env.DATABASE_URL);
+// //++++++++++++++++++++++++++++++++++++++++++
+// // DB Connection Test
+// //++++++++++++++++++++++++++++++++++++++++++
+// connection.connect((err) => {
+//   if (err) {
+//     console.log("There Is Error In DB Connection:" + err);
+//   } else {
+//     console.log("DB Connected Succefully");
+//   }
+// });
+// //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// module.exports = connection;
+// //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 const mysql = require("mysql2");
-//++++++++++++++++++++++++++++++++++++++++++
-// DB Connection
-//++++++++++++++++++++++++++++++++++++++++++
 require("dotenv").config();
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-//++++++++++++++++++++++++++++++++++++++++++
-// DB Connection Test
-//++++++++++++++++++++++++++++++++++++++++++
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT, // Add the port
+  database: process.env.DB_NAME,
+});
+
 connection.connect((err) => {
   if (err) {
-    console.log("There Is Error In DB Connection:" + err);
+    console.log("There Is Error In DB Connection: " + err);
   } else {
-    console.log("DB Connected Succefully");
+    console.log("DB Connected Successfully");
   }
 });
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 module.exports = connection;
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
