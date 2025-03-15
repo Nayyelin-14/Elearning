e3ac825388485027ba9ed95f54065aeb583fc35f
nayyelin-2002 <yelinnay4@gmail.com>
2025-03-15 11:25:15 +0700

    cahnges

--- Backend/Action/BaseUrl.js
+++ Backend/Action/BaseUrl.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 exports.getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
   //   if (process.env.VERCEL_URL) return `http://${process.env.VERCEL_URL}`;
-  return `${process.env.UI_PORT}`;
+  return "https://elearning-qjwb.vercel.app";
 };
